9c9bb4d66000aaf0430c8e7b54aa11c1e59af381
fidelret <97366867+fidelret@users.noreply.github.com>
2022-01-30 14:48:19 -0300

    Actualización total v2

--- script.js
+++ script.js
@@ -1,29 +1,90 @@
+"use strict"
 
-/*--------------------------------ENCRIPTAR MENSAJE-------------------------------*/
-document.querySelector("#btn-encriptar").addEventListener("click",function(event){ 
-    event.preventDefault();
-     var texto = document.querySelector("#input-texto").value;      
-     var textCodif = texto.replace(/e/gi,'enter').replace(/i/gi,'imes').replace(/a/gi,'ai').replace(/o/gi,'ober').replace(/u/gi,'ufat');
-         document.querySelector("#msg").value = textCodif;     
+/*------------------------------TEXTO ENTRADA-------------------------------*/
+function entrada(){
+     var input = document.querySelector("#textoCodDec");
+     return input.value;
+}
+
+/*------------------------------TEXTO SALIDA-------------------------------*/
+function salida(txtEncriptado){
+      document.querySelector("#output").value = txtEncriptado;
+}
+
+/*------------------------------BOTÓN CODIFICAR-------------------------------*/
+var botonCodif = document.querySelector("#botonCodificar");
+        botonCodif.addEventListener("click",function(event){
+        event.preventDefault(); 
+        console.log("botón presionado")
+    var texto = entrada();
+
+                                    /*---------VALIDACIÓN CODIFICAR-----------*/
+    var validacion = validarTexto(texto) 
+       if (!validacion) {
+           let txtEncriptado = encriptar(texto);
+           salida(txtEncriptado);
+       }else{
+        alert("Por favor, sólo letras minúsculas y sin tilde");
+       }      
 });
 
-/*------------------------------DESENCRIPTAR MENSAJE-------------------------------*/
-document.querySelector("#btn-desencriptar").addEventListener("click",function(event){ 
-     event.preventDefault();
-     var texto = document.querySelector("#input-texto").value;          
-     var textDecod = texto.replace(/enter/gi,'e').replace(/imes/gi,'i').replace(/ai/gi,'a').replace(/ober/gi,'o').replace(/ufat/gi,'u');      
-          document.querySelector("#msg").value = textDecod;
-         
+/*------------------------------BOTÓN DECODIFICAR-------------------------------*/
+var botonDeCodif = document.querySelector("#botonDecodificar");
+        botonDeCodif.addEventListener("click",function(event){
+        event.preventDefault(); 
+        console.log("botón presionado")
+    var texto = entrada();
+                                   /*---------VALIDACIÓN DECODIFICAR-----------*/
+    var validacion = validarTexto(texto)                         
+       if (!validacion) {
+           let txtDesencriptado = desEncriptar(texto);
+           salida(txtDesencriptado);
+       }else{
+        alert("Por favor, sólo letras minúsculas y sin tilde");
+       }      
 });
+
+/*---------------------------------VALIDACIÓN-------------------------------*/
+function validarTexto(texto){
+     var array = texto.split(''); 
+     for(var i = 0; i < array.length; i++){
+        if(array[i].codePointAt(0) != 32 && (array[i].codePointAt(0) < 97 || array[i].codePointAt(0) > 122)){   
+            return true;
+        }
+
+     }
+}
+
+/*---------------------------------ENCRIPTAR-------------------------------*/
+function encriptar(texto) {
+    var array = texto.split(''); 
+            for (var i = 0; i < array.length; i++) {
+                if (array[i] == "a") {
+                    array.splice(i, 1, "ai"); 
+                } else if (array[i] == "e") {
+                    array.splice(i, 1, "enter");
+                } else if (array[i] == "i") {
+                    array.splice(i, 1, "imes");
+                } else if (array[i] == "o") {
+                    array.splice(i, 1, "ober");
+                } else if (array[i] == "u") {
+                    array.splice(i, 1, "ufat");
+                }
+            }
+    return array.join(""); 
+}
+
+/*---------------------------------DESENCRIPTAR-------------------------------*/
+function desEncriptar(texto) {    
+        var textDec = texto.replace(/enter/gi,'e').replace(/imes/gi,'i').replace(/ai/gi,'a').replace(/ober/gi,'o').replace(/ufat/gi,'u');              
+        return textDec;
+        }
 
 /*---------------------------------BOTÓN COPIAR-------------------------------*/
  document.querySelector("#btn-copy").addEventListener("click",function (event){
-     event.preventDefault();    
-     var copiarText = document.querySelector("#msg");
-         copiarText.select();
-         document.execCommand("copy");
+    event.preventDefault();    
+    var copiarText = document.querySelector("#output");
+        copiarText.select();
+        document.execCommand("copy");
 }); 
-
-
-
- 
+    
